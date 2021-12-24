e6cde922206937adb086fa0fe8ee4d4e1045d7d5
xeemv <xeeyang08@gmail.com>
2021-12-24 00:18:37 -0600

    finished

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 const panels = document.querySelectorAll('.panel');
+/* 
+- select active classes to change the classes/IDs
+- id = .panel  
+*/
+
+panels.forEach(panel => {  
+    // looping through to all panels to call  an action
+    panel.addEventListener('click', () => {
+        // using eventlistener to call the function when a panel is clicked on
+        removeActiveClasses();
+        // this is to prevent other panels from being expanded while the selected panel being viewed
+        // this will close that panel
+        panel.classList.add('active');
+    })
+})
+
+function removeActiveClasses(){
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    })
+}
